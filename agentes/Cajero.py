836f7b3627c6acf5a6b7823cd6c82cbca690d51d
javierFili <javierfiligranaagreda@gmail.com>
2022-12-12 10:50:35 -0400

    agregando el gitignore

--- agentes/Cajero.py
+++ agentes/Cajero.py
@@ -1,27 +1,26 @@
 class Cajero:
-    dinero = 10
-    tiempo = 12
-    estado = 'inactivo'
-    costo = 5
+  dinero = 10
+  tiempo = 12
+  estado = 'inactivo'
+  costo = 5
 
-    def getEstado(self):
-        return  self.estado
+  def getEstado(self):
+    return self.estado
 
-    def setEstado(self, num):
-        if num == 1:
-            self.estado = 'inactivo'
-        elif num == 2:
-            self.estado = 'Cobrando matricula'
-        else:
-            return None
+  def setEstado(self, num):
+    if num == 1:
+      self.estado = 'inactivo'
+    elif num == 2:
+      self.estado = 'Cobrando matricula'
+    else:
+      return None
 
-    def getCosto(self):
-        return  self.costo
+  def getCosto(self):
+    return self.costo
 
-    def __init__(self):
-        self.dinero = 12
-        self.tiempo = 122
+  def __init__(self):
+    self.dinero = 12
+    self.tiempo = 122
 
-    def aunTieneDinero(self):
-        return self.dinero
-
+  def aunTieneDinero(self):
+    return self.dinero
--- main.py
+++ main.py
@@ -5,34 +5,34 @@ from agentes.Estudiante import Estudiante
 from agentes.Responsable import Responsable
 
 if __name__ == '__main__':
-    mapa = MapaUmss()
-    mapa.setPosEstudiante(0, 4)
-    mapa.setPosOficinas(4, 0)
-    mapa.setPosCajero(8, 4)
-    mapa.setPosResponsable(4,8)
+  mapa = MapaUmss()
+  mapa.setPosEstudiante(0, 4)
+  mapa.setPosOficinas(4, 0)
+  mapa.setPosCajero(8, 4)
+  mapa.setPosResponsable(4, 8)
 
-    est = Estudiante()
-    dir = Director()
-    caj = Cajero()
-    res = Responsable()
+  est = Estudiante()
+  dir = Director()
+  caj = Cajero()
+  res = Responsable()
 
-    print('---- Iteracion inicial ----')
-    print(mapa.matriz)
-    print('\n','Estudiante:'    , est.getEstado(),'\n'
-            ,'Director:'    , dir.getEstado(),'\n'
-            ,'Cajero:'      , caj.getEstado(),'\n'
-            ,'Responsable:' , res.getEstado())
+  print('---- Iteracion inicial ----')
+  print(mapa.matriz)
+  print('\n', 'Estudiante:', est.getEstado(), '\n'
+        , 'Director:', dir.getEstado(), '\n'
+        , 'Cajero:', caj.getEstado(), '\n'
+        , 'Responsable:', res.getEstado())
 
-    g = 1
+  g = 1
 
-    while est.getMaterias() > 0:
-        if est.pagar_a(caj):
-                est.setEstado(2)
-                caj.setEstado(2)
-                print('---- Iteracion ',g,' ----')
-                print(mapa.matriz)
-                print('\n','Estudiante:'    , est.getEstado(),'\n'
-                        ,'Director:'    , dir.getEstado(),'\n'
-                        ,'Cajero:'      , caj.getEstado(),'\n'
-                        ,'Responsable:' , res.getEstado())
-        est.resMaterias()
+  while est.getMaterias() > 0:
+    if est.pagar_a(caj):
+      est.setEstado(2)
+      caj.setEstado(2)
+      print('---- Iteracion ', g, ' ----')
+      print(mapa.matriz)
+      print('\n', 'Estudiante:', est.getEstado(), '\n'
+            , 'Director:', dir.getEstado(), '\n'
+            , 'Cajero:', caj.getEstado(), '\n'
+            , 'Responsable:', res.getEstado())
+    est.resMaterias()
